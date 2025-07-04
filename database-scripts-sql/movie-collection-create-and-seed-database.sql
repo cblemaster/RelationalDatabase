2663c5d65cfdf7ae5b7fdc5d7f4dd6dcafe1e05b
Brian LeMaster <brian.lemaster@outlook.com>
2025-07-04 12:36:33 -0400

    db script: drop and create db

--- database-scripts-sql/movie-collection-create-and-seed-database.sql
+++ database-scripts-sql/movie-collection-create-and-seed-database.sql
@@ -1,6 +1,28 @@
 
--- drop, create, use database (.25h)
--- save to repo and push (.25h)
+USE master
+GO
+
+DECLARE @sql nvarchar(1000);
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = N'MovieCollection')
+
+BEGIN
+    SET @sql = N'USE MovieCollection;
+
+                 ALTER DATABASE MovieCollection SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+                 USE master;
+
+                 DROP DATABASE MovieCollection;';
+    EXEC (@sql);
+END;
+
+CREATE DATABASE MovieCollection
+GO
+
+USE MovieCollection
+GO
+
+
 -- create tables (1.5h)
 -- seed data when tables created (1.5h)
 -- define and create views (4h)
