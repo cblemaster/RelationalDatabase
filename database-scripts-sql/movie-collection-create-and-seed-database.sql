499adcde046c4a465b00a778f801865867cfc8b2
Brian LeMaster <brian.lemaster@outlook.com>
2025-07-04 12:30:02 -0400

    add db create script

---
+++ database-scripts-sql/movie-collection-create-and-seed-database.sql
@@ -0,0 +1,10 @@
+
+-- drop, create, use database (.25h)
+-- save to repo and push (.25h)
+-- create tables (1.5h)
+-- seed data when tables created (1.5h)
+-- define and create views (4h)
+-- define and create sprocs (4h)
+-- define and create tests (8h)
+-- does the script need to be split up? (0-8h)
+-- max effort 27.5h
